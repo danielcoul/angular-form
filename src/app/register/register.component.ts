8c86ee0e5503f86e94575287de7d42df24077a9a
danielcoul <coulibalydaniel1@gmail.com>
2023-10-04 13:49:51 +0000

    actualisation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from "@angular/forms";
+import { FormGroup, FormControl, FormBuilder, Validators } from "@angular/forms";
 import { User } from './user';
 @Component({
   selector: 'app-register',
@@ -9,21 +9,40 @@ import { User } from './user';
 export class RegisterComponent implements OnInit{
 
   public user: User = new User();
-  public registrerForm!: FormGroup;
+  public registerForm!: FormGroup;
 
-  constructor(){}
+  constructor(private  fb: FormBuilder){}
 
   ngOnInit(): void {
-     this.registrerForm = new FormGroup({
-       firstName: new FormControl(),
-       latsName: new FormControl(),
-       email: new FormControl(),
-       sendCatalog : new FormControl(true)
-     });
+
+    this.registerForm =  this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(4)]],
+      latsName: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(10)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: '',
+      notification: 'email',
+      sendCatalog: false,
+    });
+    //  this.registerForm = new FormGroup({
+    //    firstName: new FormControl(),
+    //    latsName: new FormControl(),
+    //    email: new FormControl(),
+    //    sendCatalog : new FormControl(true)
+    //  });
   }
 
   public saveData(){
-    console.log(this.registrerForm);
-    console.log('valeurs:', JSON.stringify(this.registrerForm));
+    console.log(this.registerForm);
+    console.log('valeurs:', JSON.stringify(this.registerForm.value));
+  }
+
+  public fillFormData(): void {
+    this.registerForm.setValue({
+      firstName: 'John',
+      latsName: 'Daniel',
+      email: 'daniel@gmail.com',
+      sendCatalog: true
+    })
   }
 }
+
